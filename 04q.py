28018d145bea5c788b50830e9e154224063ad9c2
sanjana94-82 <140141785+sanjana94-82@users.noreply.github.com>
2025-02-26 13:08:57 +0530

    Delete 04q.py

--- 04q.py
+++
@@ -1,8 +0,0 @@
-#highest scorer
-def highest_scorer(o_dict):
-    a=max(o_dict.values())
-    for k,v in o_dict.items():
-        if v==a:
-            return k,v
-d={"Ramesh":900,"Vicky":800,"Ritish":1900}
-print(highest_scorer(d))
